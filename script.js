3f17cba4f2c0234a62b20e1ee0caaa6dfc0014a0
Juan S Silva <juanse909@hotmail.com>
2024-04-05 18:14:09 -0500

    Syntax error fix in JS

--- script.js
+++ script.js
@@ -1,77 +1,76 @@
 const render = () => {
+  const user1SelectorBtn = document.querySelector("#user1-selector");
+  const user2SelectorBtn = document.querySelector("#user2-selector");
+  const chatHeader = document.querySelector(".chat-header");
+  const chatMessages = document.querySelector(".chat-messages");
+  const chatInputForm = document.querySelector(".chat-input-form");
+  const chatInput = document.querySelector(".chat-input");
+  const clearChatBtn = document.querySelector(".clear-chat-button");
 
-const user1SelectorBtn = document.querySelector('#user1-selector');
-const user2SelectorBtn = document.querySelector('#user2-selector');
-const chatHeader = document.querySelector('.chat-header');
-const chatMessages = document.querySelector('.chat-messages');
-const chatInputForm = document.querySelector('.chat-input-form');
-const chatInput = document.querySelector('.chat-input');
-const clearChatBtn = document.querySelector('.clear-chat-button');
-
-const messages = JSON.parse(localStorage.getItem('messages')) || [];
-
-const createChatMessageElement = (message) => `
-    <div class="message ${message.sender === 'Usuario I' ? 'red-bg' : 'gray-bg'}"> 
-        <div class="message-sender">${message.sender}</div>
-        <div class="message-text">${message.text}</div>
-        <div class="message-timestamp">${message.timestamp}</div>
+  const messages = JSON.parse(localStorage.getItem("messages")) || [];
+  const createChatMessageElement = (message) => `
+  <div class="message ${
+      message.sender === "Usuario I" ? "red-bg" : "gray-bg"
+    }"> 
+    <div class="message-sender">${message.sender}</div>
+    <div class="message-text">${message.text}</div>
+    <div class="message-timestamp">${message.timestamp}</div>
     </div>
-`;
-
-window.onload = () => {
+    `;
     messages.forEach((message) => {
-        chatMessages.innerHTML += createChatMessageElement(message);
+      chatMessages.innerHTML += createChatMessageElement(message);
     });
-};
 
-let messageSender = 'Usuario I';
+  let messageSender = "Usuario I";
 
-const updateMessageSender = (name) => {
+  const updateMessageSender = (name) => {
     messageSender = name;
     chatHeader.innerText = `${messageSender} esta escribiendo...`;
     chatInput.placeholder = `Escriba aquí, ${messageSender}`;
 
-    if (name === 'Usuario I') {
-        user1SelectorBtn.classList.add('active-person');
-        user2SelectorBtn.classList.remove('active-person');
+    if (name === "Usuario I") {
+      user1SelectorBtn.classList.add("active-person");
+      user2SelectorBtn.classList.remove("active-person");
     }
-    if (name === 'Usuario II') {
-        user1SelectorBtn.classList.remove('active-person');
-        user2SelectorBtn.classList.add('active-person');
+    if (name === "Usuario II") {
+      user1SelectorBtn.classList.remove("active-person");
+      user2SelectorBtn.classList.add("active-person");
     }
 
     chatInput.focus();
-};
+  };
 
-user1SelectorBtn.onclick = () => updateMessageSender('Usuario I');
-user2SelectorBtn.onclick = () => updateMessageSender('Usuario II');
+  user1SelectorBtn.onclick = () => updateMessageSender("Usuario I");
+  user2SelectorBtn.onclick = () => updateMessageSender("Usuario II");
 
-
-const sendMessage = (e) => {
+  const sendMessage = (e) => {
     e.preventDefault();
 
-    const timestamp = new Date().toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+    const timestamp = new Date().toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
     const message = {
-        sender: messageSender,
-        text: chatInput.value,
-        timestamp,
+      sender: messageSender,
+      text: chatInput.value,
+      timestamp,
     };
 
     messages.push(message);
-    localStorage.setItem('messages', JSON.stringify(messages));
+    localStorage.setItem("messages", JSON.stringify(messages));
     chatMessages.innerHTML += createChatMessageElement(message);
 
     chatInputForm.reset();
     chatMessages.scrollTop = chatMessages.scrollHeight;
-};
+  };
 
-chatInputForm.addEventListener('submit', sendMessage);
+  chatInputForm.addEventListener("submit", sendMessage);
 
-clearChatBtn.addEventListener('click', () => {
+  clearChatBtn.addEventListener("click", () => {
     localStorage.clear();
-    chatMessages.innerHTML = '';
-});
-
+    chatMessages.innerHTML = "";
+  });
 };
 
 document.addEventListener("DOMContentLoaded", render);
